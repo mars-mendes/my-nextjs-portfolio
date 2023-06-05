8208dcfeba8ab4c3e8800131d3000622f82631be
Mars Mendes <mars.mendes@ibm.com>
2023-06-05 19:04:36 -0300

    added docs file for pages preview

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// @ts-check
 
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+    output: 'export',
+    basePath: '/my-nextjs-portfolio',
+    distDir: 'dist'
+  }
+  
+  module.exports = nextConfig
