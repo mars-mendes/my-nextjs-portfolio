a9e359a416bce58c73b0554b5a5e75ba7238c260
Mars Mendes <mars.mendes@ibm.com>
2023-06-06 14:39:11 -0300

    next config edit

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const path = require('path');
 
 const nextConfig = {
     output: 'export',
-    assetPrefix: "https://mars-mendes.github.io/my-nextjs-portfolio/",
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
       },
